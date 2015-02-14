412cdbc6817c38a7434bdec96642f3c934710c36
Abdullah <abdullahoguk@users.noreply.github.com>
2015-02-14 10:10:40 +0200

    Create FullStackException.java

---
+++ processes/FullStackException.java
@@ -0,0 +1,8 @@
+package processes;
+public class FullStackException extends RuntimeException { 
+	private static final long serialVersionUID = 3173909821336481006L;
+
+	public FullStackException(String err) {
+	    super(err);
+	  }
+	}
