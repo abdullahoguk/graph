cbac587280bc5bb068e424aa5127c4c822492e34
Abdullah <abdullahoguk@users.noreply.github.com>
2015-02-16 08:49:08 +0200

    Create MinSpanTree.java

---
+++ processes/MinSpanTree.java
@@ -0,0 +1,17 @@
+package processes;
+
+public class MinSpanTree {
+	
+	NodeQueue<SLinkedList<Edge>> q;
+	String result;
+	Boolean[] visitStates;
+	SLinkedList<Edge> currList;
+	int currEdge;
+	
+	public MinSpanTree(WeightedGraph g) {
+	
+		
+		
+	}
+
+}
